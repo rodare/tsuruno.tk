a94730e8170e3d66cb777e99c8147d671cfb1ceb
Rodrigo <rodare.cl@gmail.com>
2020-09-28 16:32:03 +0200

    cambios

--- milko-grozeb/indiv.js
+++ milko-grozeb/indiv.js
@@ -5,8 +5,12 @@
 // var cityMember = document.getElementById("cityMember").innerHTML = output="Kraków";
 // var memberName = document.getElementById("memberName").innerHTML = output2= "Milko Grozeb";
 
-var name = document.getElementById("memberName").innerHTML = "Milko Grozeb";
+var name = document.getElementById("memberName").innerHTML = "Your Name Here";
+var email = document.getElementById("memberEmail").innerHTML = "rodare.cl@gmail.com";
 
+function pruebaa {
+    document.getElementById("pruebaa").innerHTML = "Hola";
+}
 
 // function writeName(){
 //   var b=document.getElementById("input3").value; 
