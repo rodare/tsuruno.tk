ced9327b779823e391debfeef5eea9f7e58c27d6
Rodrigo <rodare.cl@gmail.com>
2020-09-30 21:33:04 +0200

    funciones1

--- milko-grozeb/indiv.js
+++ milko-grozeb/indiv.js
@@ -5,8 +5,21 @@
 // var cityMember = document.getElementById("cityMember").innerHTML = output="Kraków";
 // var memberName = document.getElementById("memberName").innerHTML = output2= "Milko Grozeb";
 
-var name = document.getElementById("memberName").innerHTML = "Your Name Here";
-var email = document.getElementById("memberEmail").innerHTML = "rodare.cl@gmail.com";
+function memberName(){
+    document.write("Milko Grozeb");
+} 
+
+function memberEmail(){
+    document.write("milko.grozeb@gmail.com");
+} 
+
+
+// let name = document.getElementById("memberName").innerHTML = "Milko Grozeb";
+//     name2 = document.getElementById("memberName2").innerHTML = "Milko Grozeb";
+//     email = document.getElementById("memberEmail").innerHTML = "Your Email Here";
+
+// To paste your calendar, get its Public URL following these instructions (https://support.google.com/calendar/answer/41207?hl=es) and paste in calendario.html line 67.
+
 
 // function writeName(){
 //   var b=document.getElementById("input3").value; 
