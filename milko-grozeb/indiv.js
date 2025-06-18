acc13f579071c3b9e4b9ff281ccffa253bd9d8d2
Rodrigo Arenas Catalán <rodare.cl@gmail.com>
2025-06-18 14:49:24 +0200

    nuevo

---
+++ milko-grozeb/indiv.js
@@ -0,0 +1,71 @@
+
+
+
+// let countryCity = document.getElementById("countryCity").innerHTML = input= "POLAND";
+// var cityMember = document.getElementById("cityMember").innerHTML = output="Kraków";
+// var memberName = document.getElementById("memberName").innerHTML = output2= "Milko Grozeb";
+
+var myName = "Milko Grozeb"
+function memberName(){
+    document.write(myName);
+}
+
+function memberEmail(){
+    document.write("milko.grozeb@gmail.com");
+}
+
+function memberShare(){
+    document.write("I can share potatos, bike tips, good company.");
+}
+
+function memberLike(){
+    document.write("I like outdoor activities, bicycles, bike trips, .");
+}
+
+function memberWant(){
+    document.write("I would like to learn how to play the guitar, to do trips with friends, to learn about ceramics, to learn a programming language.");
+}
+
+
+function pressed(){
+    var text = document.getElementById("inp").value;
+    hola.innerHTML=text
+    hola.style.color="green";
+}
+
+
+// var arr = ["hola", 1, 2, 5, "yo"]
+// var i = 0
+// while (i<arr.length){
+//     console.log(arr[0])
+//     i++
+// }
+
+// var arr=["hey","perrito","quiuvo",2,4,3]
+// for(var i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+
+// }
+
+// var arr =[]
+
+// var i = 0
+// while (i <= 100){
+//     arr.push(i)
+//     i++
+// }
+
+
+
+
+// let name = document.getElementById("memberName").innerHTML = "Milko Grozeb";
+//     name2 = document.getElementById("memberName2").innerHTML = "Milko Grozeb";
+//     email = document.getElementById("memberEmail").innerHTML = "Your Email Here";
+
+// To paste your calendar, get its Public URL following these instructions (https://support.google.com/calendar/answer/41207?hl=es) and paste in calendar.html line 67.
+
+
+// function writeName(){
+//   var b=document.getElementById("input3").value;
+//   document.getElementById("output3").innerHTML=b;
+// }
